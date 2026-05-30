350c1035d24a7648aa3b9669d924bb6004b07d4b
Luca Paliska <lucarpaliska@gmail.com>
2026-05-30 16:50:37 -0700

    power menu style changes and rofi menu addition.

--- home/config/waybar/scripts/power_menu/power_menu.sh
+++ home/config/waybar/scripts/power_menu/power_menu.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-choice=$(printf "Shutdown\0icon\x1fsystem-shutdown\nReboot\0icon\x1fsystem-reboot\nSuspend\0icon\x1fsystem-suspend\nLogout\0icon\x1fsystem-log-out\n" | \
+choice=$(printf "Shutdown\0icon\x1fsystem-shutdown\nReboot\0icon\x1fsystem-reboot\nSuspend\0icon\x1fsystem-suspend\nHibernate\0icon\x1fsystem-hibernate\nLogout\0icon\x1fsystem-log-out\n" | \
     rofi -dmenu -i \
     -p "" \
-    -theme-str 'window {width: 20%;} listview {lines: 4;} element-icon {size: 26px; margin: 0 8 0 16;}' | tr -d '\n\r')
+    -theme-str 'window {width: 20%;} listview {lines: 5;} element-icon {size: 26px; margin: 0 8 0 16;}' | tr -d '\n\r')
 
 case "$choice" in
-    Shutdown) systemctl poweroff ;;
-    Reboot)   systemctl reboot ;;
-    Suspend)  systemctl suspend ;;
-    Logout)   hyprctl dispatch exit ;;
+    Shutdown)  systemctl poweroff ;;
+    Reboot)    systemctl reboot ;;
+    Suspend)   systemctl suspend ;;
+    Hibernate) systemctl hibernate ;;
+    Logout)    hyprctl dispatch exit ;;
 esac
