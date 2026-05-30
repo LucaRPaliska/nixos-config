d9be32ca85e96d2fd29372c62dade3c6e2bbb240
Luca Paliska <lucarpaliska@gmail.com>
2026-05-30 16:49:15 -0700

    power menu style changes and rofi menu addition.

--- home/config/waybar/scripts/power_menu/power_menu.sh
+++ home/config/waybar/scripts/power_menu/power_menu.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 choice=$(printf "Shutdown\0icon\x1fsystem-shutdown\nReboot\0icon\x1fsystem-reboot\nSuspend\0icon\x1fsystem-suspend\nLogout\0icon\x1fsystem-log-out\n" | \
-    rofi -dmenu \
+    rofi -dmenu -i \
     -p "" \
-    -theme-str 'window {width: 20%;} listview {lines: 4;} element-icon {size: 26px; margin: 0 8 0 16;}' | tr -d '\n')
+    -theme-str 'window {width: 20%;} listview {lines: 4;} element-icon {size: 26px; margin: 0 8 0 16;}' | tr -d '\n\r')
 
 case "$choice" in
-    Shutdown|shutdown) systemctl poweroff ;;
-    Reboot|reboot)     systemctl reboot ;;
-    Suspend|suspend)   systemctl suspend ;;
-    Logout|logout)     hyprctl dispatch exit ;;
+    Shutdown) systemctl poweroff ;;
+    Reboot)   systemctl reboot ;;
+    Suspend)  systemctl suspend ;;
+    Logout)   hyprctl dispatch exit ;;
 esac
