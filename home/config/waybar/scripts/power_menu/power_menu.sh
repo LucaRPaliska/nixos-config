1ae05c197a30e8155f061ddba59ae448bfffb220
Luca Paliska <lucarpaliska@gmail.com>
2026-05-30 20:04:24 -0700

    power menu on Super + ESC

--- home/config/waybar/scripts/power_menu/power_menu.sh
+++ home/config/waybar/scripts/power_menu/power_menu.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-choice=$(printf "Shutdown\0icon\x1fsystem-shutdown\nReboot\0icon\x1fsystem-reboot\nSuspend\0icon\x1fsystem-suspend\nHibernate\0icon\x1fsystem-hibernate\nLogout\0icon\x1fsystem-log-out\n" | \
-    rofi -dmenu -i \
-    -p "" \
-    -theme-str 'window {width: 20%;} listview {lines: 5;} element-icon {size: 26px; margin: 0 8 0 16;}' | tr -d '\n\r')
+options="Shutdown\0icon\x1fsystem-shutdown\n"
+options+="Reboot\0icon\x1fsystem-reboot\n"
+options+="Suspend\0icon\x1fsystem-suspend\n"
+options+="Hibernate\0icon\x1fsystem-hibernate\n"
+options+="Logout\0icon\x1fsystem-log-out\n"
+
+choice=$(printf "$options" | rofi -dmenu -i -p "" \
+    -theme-str 'window {width: 20%;} listview {lines: 5;} element-icon {size: 26px; margin: 0 8 0 16;}' \
+    | tr -d '\n\r')
 
 case "$choice" in
     Shutdown)  systemctl poweroff ;;
